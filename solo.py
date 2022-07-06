fd9946c0a0c2febc7075475aaa3cb4924b0c6506
Error <88781785+TheSadError@users.noreply.github.com>
2022-07-06 11:17:42 +0400

    Update solo.py

--- solo.py
+++ solo.py
@@ -7,7 +7,6 @@ import sys
 import core.pscan as pscan
 import core.oscan as oscan
 import core.swordlist as swordlist
-import core.dork as dork
 import datetime
 
 def progress(percent=0, width=40):
@@ -23,14 +22,13 @@ def load():
         progress(i)
         sleep(0.01)
 
-def start(us,ps,os,wr,dr):
+def start(us,ps,os,wr):
     if(ps == "true"):
         pscan.portscan(us)
     if(os == "true"):
         oscan.oscan(us)
-    if(wr == "start"):
-        swordlist.create(wr)
-    dork.ls(dr)
+    if(wr == "start" or wr == "true"):
+        swordlist.create()
         
 if __name__ == "__main__":
     e = datetime.datetime.now()
@@ -54,15 +52,13 @@ if __name__ == "__main__":
     parser.add_option("--u","--U",dest = "url",type="string",help = "Target URL , Username Usage : --u TheSadError , --u thesaderror.com")
     parser.add_option("--o","--osint",dest = "osint",type="string",help = "Osint Option Usage : --o true")
     parser.add_option("--w","--word",dest = "wordlist",type="string",help = "Create big wordlist option : --w start")
-    parser.add_option("--d","--dork",dest = "dork",type="int",help = "List vulnerable websites list for hack easly by google dorking : --d number of websites to list ")
     (options,args) = parser.parse_args()
     port = options.port
     url = options.url
     osint = options.osint
     wordlist = options.wordlist
-    dork = options.dork
     print("Starting solo scanner 2.3 ( https://github.com/TheSadError/solo ) at %s" % e)
-    if(url == None and port == None and osint == None and wordlist == None and dork == None):
+    if(url == None and port == None and osint == None and wordlist == None ):
         print(parser.usage)
         exit(0)
-    start(url,port,osint,wordlist,dork)
+    start(url,port,osint,wordlist)
